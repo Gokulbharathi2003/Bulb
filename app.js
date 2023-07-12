b0efff70ac25e44118958a080a1d39d35e58b522
Gokulbharathi2003 <139205885+Gokulbharathi2003@users.noreply.github.com>
2023-07-12 11:30:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var on_btn = document.getElementById("on");
+var off_btn =document.getElementById("off");
+var img = document.getElementById("bulb")
+function bulb_on(){
+    img.src="https://previews.123rf.com/images/murika/murika1511/murika151100069/48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.jpg"
+}
+
+function bulb_off(){
+    img.src=" https://m.media-amazon.com/images/I/51bCZTw1HlL._AC_UF1000,1000_QL80_.jpg"
+}
